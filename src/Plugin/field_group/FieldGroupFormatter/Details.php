9592a7709b9ac20c2ff7eeaea42bcc661cb0466f
webflo <webflo@254778.no-reply.drupal.org>
2014-10-01 18:14:41 +0200

    Added Details element

---
+++ src/Plugin/field_group/FieldGroupFormatter/Details.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+/**
+ * @file
+ * Contains \Drupal\field_group\Plugin\field_group\FieldGroupFormatter\Details.
+ */
+
+namespace Drupal\field_group\Plugin\field_group\FieldGroupFormatter;
+
+use Drupal\Component\Utility\String;
+use Drupal\field_group\FieldGroupFormatterBase;
+
+/**
+ * Details element.
+ *
+ * @FieldGroupFormatter(
+ *   id = "details",
+ *   label = @Translation("Details"),
+ *   description = @Translation("Add a details element"),
+ *   supported_contexts = {
+ *     "form",
+ *     "view"
+ *   }
+ * )
+ */
+class Details extends FieldGroupFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm() {
+    $form = parent::settingsForm();
+
+    $form['open'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Display element open by default.'),
+      '#default_value' => $this->getSetting('open'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+
+    $summary = array();
+    if ($this->getSetting('open')) {
+      $summary[] = \Drupal::translation()->translate('Default state open');
+    }
+    else {
+      $summary[] = \Drupal::translation()->translate('Default state closed');
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preRender($element) {
+    $element += array(
+      '#type' => 'details',
+      '#title' => String::checkPlain(\Drupal::translation()->translate($this->group->label)),
+      '#attributes' => array('class' => explode(' ', $this->group->classes)),
+      '#description' => $this->group->description,
+      '#open' => $this->getSetting('open')
+    );
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      'open' => FALSE,
+    ) + parent::defaultSettings();
+  }
+
+}
