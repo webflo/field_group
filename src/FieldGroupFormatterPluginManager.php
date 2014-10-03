ff16580a69ba309c1608a1b819e4cd4e62cf438f
zuuperman <nils.destoop@gmail.com>
2014-10-03 10:47:06 +0200

    Convert form_state to FormState

--- src/FieldGroupFormatterPluginManager.php
+++ src/FieldGroupFormatterPluginManager.php
@@ -25,13 +25,15 @@ class FieldGroupFormatterPluginManager extends DefaultPluginManager {
    *   keyed by the corresponding namespace to look for plugin implementations.
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
    *   Cache backend instance to use.
+   * @param \Drupal\Core\Language\LanguageManager $language_manager
+   *   The language manager.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
    */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
-    parent::__construct('Plugin/field_group/FieldGroupFormatter', $namespaces, $module_handler, 'Drupal\field_group\Annotation\FieldGroupFormatter');
+    parent::__construct('Plugin/field_group/FieldGroupFormatter', $namespaces, $module_handler, 'Drupal\field_group\FieldGroupFormatterInterface', 'Drupal\field_group\Annotation\FieldGroupFormatter');
 
-    $this->alterInfo('field_group_formatter');
+    $this->alterInfo('field_group_formatter_info');
     $this->setCacheBackend($cache_backend, 'field_group_formatter_info');
   }
 
