792a54dd943a9c77bbae93b674887f860cad4973
Florian Weber <florian.weber@ueberbit.de>
2015-07-03 01:46:33 +0200

    Replace String::checkPlain with SafeMarkup::checkPlain, element_children with Element::children, fix entiy view display edit form

--- src/Plugin/field_group/FieldGroupFormatter/Details.php
+++ src/Plugin/field_group/FieldGroupFormatter/Details.php
@@ -8,7 +8,9 @@
 
 namespace Drupal\field_group\Plugin\field_group\FieldGroupFormatter;
 
-use Drupal\Component\Utility\String;
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\SafeMarkup;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\field_group\FieldGroupFormatterBase;
 
 /**
@@ -33,12 +35,12 @@ class Details extends FieldGroupFormatterBase {
 
     $element += array(
       '#type' => 'details',
-      '#title' => String::checkPlain(\Drupal::translation()->translate($this->getLabel())),
+      '#title' => SafeMarkup::checkPlain(\Drupal::translation()->translate($this->getLabel())),
       '#open' => $this->getSetting('open')
     );
 
     if ($this->getSetting('id')) {
-      $element['#id'] = drupal_html_id($this->getSetting('id'));
+      $element['#id'] = Html::getId($this->getSetting('id'));
     }
 
     if ($this->getSetting('classes')) {
